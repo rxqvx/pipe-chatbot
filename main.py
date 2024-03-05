9fb74d63a95790a8a410c748eb2f457ad99f64a1
Fernando Lin <91858664+Fernandolin@users.noreply.github.com>
2024-03-05 20:28:45 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+#Henrique Kenji 22.121.030-5
+#Fernando Chan Lin 22.221.015-5
+#Erico Medeiros Correia da Silva 22.221.051-0
+
 from chatbot import ChatBot
 myChatBot = ChatBot()
 #apenas carregar um modelo pronto
@@ -12,13 +17,13 @@ myChatBot.createModel()
 print("Bem vindo ao Chatbot PIPE! Minha função é tirar suas dúvidas sobre o programa!")
 
 
-pergunta = input("como posso te ajudar?")
+pergunta = input("\ncomo posso te ajudar?\n")
 resposta, intencao = myChatBot.chatbot_response(pergunta)
 print(resposta + "   ["+intencao[0]['intent']+"]")
 
 
 while (intencao[0]['intent']!="despedida"):
-    pergunta = input("posso lhe ajudar com algo a mais?")
+    pergunta = input("\nposso lhe ajudar com algo a mais?\n")
     resposta, intencao = myChatBot.chatbot_response(pergunta)
     print(resposta + "   [" + intencao[0]['intent'] + "]")
 
